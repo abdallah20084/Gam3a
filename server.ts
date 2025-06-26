ceb29bae7c03cc77dcf36ea6742d2e3f0ad2b4db
Abdallah Net <50876237+abdallah20084@users.noreply.github.com>
2025-06-26 17:27:34 +0300

    nice

--- server.ts
+++ server.ts
@@ -103,8 +103,7 @@ app.prepare().then(() => {
     pingInterval: 25000,
     cors: {
       origin: process.env.NODE_ENV === 'production'
-        ? ["https://yourdomain.com"]
-        : ["http://localhost:3000", "http://127.0.0.1:3000"],
+         ["http://localhost:3001", "http://127.0.0.1:3001"],
       methods: ["GET", "POST"],
       credentials: true,
       allowedHeaders: ["authorization", "content-type"]    },
